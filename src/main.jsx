3bc0ad54c48ee83e73ed9d1807211aa88c1ed184
Anton Vagin <anton.vagin87@gmail.com>
2025-11-24 02:42:40 +0000

    Build error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import TravelApp from './TravelPlanner.jsx'
-import './index.css' // Optional if you have custom CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
